3bd43caf789aa2c6b09b3b28fec54dd32e6037af
gladja <gladja@yandex.ru>
2023-01-04 08:51:57 +0200

    refactor form input style

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,15 @@
           <label for="wallet" class="block text-sm font-medium text-gray-700"
           >Тикер</label
           >
-          <div class="mt-1 relative rounded-md shadow-md">
+          <div class="mt-1  relative rounded-md shadow-md">
             <input
                 v-model="ticker"
                 v-on:keydown.enter="add"
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
+                class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="Например BTC"
             />
           </div>
           <div v-if="ticker" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
