8bb40a1bfe3e58c9b932f88790167c5dfa3dd7fc
gladja <gladja@yandex.ru>
2023-01-05 20:00:15 +0200

    refactor code

--- src/App.vue
+++ src/App.vue
@@ -98,10 +98,10 @@
 
     <hr v-if="tickers.length" class="w-full border-t border-gray-600 my-4"/>
     <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-      <div v-for="item in filteredTickers()"
+      <div v-for="item in paginatedTickers"
            v-bind:key="item.name"
            @click="select(item)"
-           :class="sel === item ? 'border-4' : ''"
+           :class="selectedTicker === item ? 'border-4' : ''"
            class="bg-white overflow-hidden shadow rounded-lg border-purple-800  border-solid cursor-pointer"
       >
         <div class="px-4 py-5 sm:p-6 text-center">
@@ -135,13 +135,13 @@
       </div>
     </dl>
     <hr v-if="tickers.length" class="w-full border-t border-gray-600 my-4"/>
-    <section v-if="sel" class="relative">
+    <section v-if="selectedTicker" class="relative">
       <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{ sel.name }} - USD
+        {{ selectedTicker.name }} - USD
       </h3>
       <div class="flex items-end border-gray-600 border-b border-l h-64">
         <div
-            v-for="(bar, idx) in normalizeGraph()"
+            v-for="(bar, idx) in normalizeGraph"
             :key="idx"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
@@ -157,7 +157,7 @@
         <!--        ></div>-->
       </div>
       <button
-          @click="sel = null"
+          @click="selectedTicker = null"
           type="button"
           class="absolute top-0 right-0"
       >
@@ -193,25 +193,24 @@ export default {
   data() {
     return {
       ticker: null,
+      filter: '',
       tickers: [],
-      sel: null,
+      selectedTicker: null,
       graph: [],
       page: 1,
-      filter: '',
-      hasNextPage: true,
     }
   },
 
   created() {
-    const windowData = Object.formEntries(new URL(window.location).searchParams.entries());
-
-    if (windowData.filter) {
-      this.filter = windowData.filter;
-    }
-
-    if (windowData.page) {
-      this.filter = windowData.page;
-    }
+    // const windowData = Object.formEntries(new URL(window.location).searchParams.entries());
+    //
+    // if (windowData.filter) {
+    //   this.filter = windowData.filter;
+    // }
+    //
+    // if (windowData.page) {
+    //   this.filter = windowData.page;
+    // }
 
     const tickersData = localStorage.getItem('cryptonomicon-list');
 
@@ -223,17 +222,38 @@ export default {
     }
   },
 
-  methods: {
-
-    filteredTickers() {
-      const start = (this.page - 1) * 6;
-      const end = this.page * 6;
-
-      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter.toUpperCase()));
-      this.hasNextPage = filteredTickers.length > end;
-      return filteredTickers.slice(start, end);
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6
     },
 
+    endIndex() {
+      return this.page * 6
+    },
+
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter.toUpperCase()));
+    },
+
+    paginatedTickers() {
+    return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex
+    },
+
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      if (maxValue === minValue) {
+        return this.graph.map(() => 50);
+      }
+      return this.graph.map(price => 5 + ((price - minValue) * 95) / (maxValue - minValue));
+    }
+  },
+
+  methods: {
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -243,7 +263,7 @@ export default {
         this.tickers.find(t => t.name === tickerName).price =
             data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
 
-        if (this.sel?.name === tickerName) {
+        if (this.selectedTicker?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 3000);
@@ -263,22 +283,28 @@ export default {
     },
 
     select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
+      this.selectedTicker = ticker;
     },
 
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(item => item != tickerToRemove);
+       if (this.selectedTicker === tickerToRemove) {
+         this.selectedTicker = null;
+       }
     },
-
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(price => 5 + ((price - minValue) * 95) / (maxValue - minValue));
-    }
   },
 
   watch: {
+    selectedTicker() {
+      this.graph = [];
+    },
+
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },
+
     filter() {
       this.page = 1;
       window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`);
